d0b6b260a4c37b5dd3386fddd9d5cd1792acee34
barni211 <barni211@gmail.com>
2017-09-14 19:24:01 +0200

    Added pagination

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -4,14 +4,43 @@ using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
+
 
 namespace SportsStore.UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        [TestMethod]
+        public void Can_Pagination_View_Model()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId = 1, Name = "P1" },
+                new Product {ProductId = 2, Name = "P2" },
+                new Product {ProductId = 3, Name = "P3" },
+                new Product {ProductId = 4, Name = "P4" },
+                new Product {ProductId = 5, Name = "P5" }
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
+
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(2, pageInfo.CurrentPage);
+            Assert.AreEqual(3, pageInfo.ItemsPerPage);
+            Assert.AreEqual(5, pageInfo.TotalItems);
+            Assert.AreEqual(2, pageInfo.TotalPages);
+        }
+
         [TestMethod]
         public void Can_Paginate()
         {
@@ -29,12 +58,28 @@ namespace SportsStore.UnitTests
             ProductController controller= new ProductController(mock.Object);
             controller.PageSize = 3;
 
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
 
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual("P4", prodArray[0].Name);
             Assert.AreEqual("P5", prodArray[1].Name);
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            HtmlHelper myHelper = null;
+            PagingInfo pagingInfo= new PagingInfo { CurrentPage = 2, TotalItems = 28, ItemsPerPage = 10 };
+
+            Func<int, string> pageUrlDelegate = i => "Strona" + i;
+
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a>"
+            + @"<a class=""btn btn-default btn-primary selected"" href=""Strona2"">2</a>"
+            + @"<a class=""btn btn-default"" href=""Strona3"">3</a>", result.ToString());
+
         }
     }
 }
